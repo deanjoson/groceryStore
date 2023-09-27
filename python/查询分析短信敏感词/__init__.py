84295d38e48328d92ca440fa50b3746252df9a59
邓军 <dengjun@coolwang.cn>
2023-09-27 17:31:33 +0800

    循环调用API接口获取内容中的敏感词

--- python/查询分析短信敏感词/__init__.py
+++ python/查询分析短信敏感词/__init__.py
@@ -4,6 +4,9 @@
 # @time    : 2023/9/27 16:59
 # @function: 从数据库短信表中查询历史所有短信内容，逐条调用接口查询是否包含敏感词，并将结果输出到文件进行分析
 # @version : V1.0.0
+import json
+
+import requests
 
 from python.util.sql_helper import SqlHelper
 
@@ -19,5 +22,19 @@ select_sql = f'SELECT notice_id,content FROM db_notice.t_sms '
 results = sql_helper.query_dict(select_sql)
 
 for result in results:
-    # 打印短信内容
-    print(result['content'])
+    data = {
+        "text": result['content']
+    }
+    headers = {
+        "Content-Type": "application/json",
+    }
+    response = requests.post(
+        url='http://127.0.0.1:8003/sensitive/open/findAll',
+        headers=headers,
+        data=json.dumps(data)
+    )
+    # 响应成功
+    if response.status_code == 200:
+        response_json = json.loads(response.text)
+        sensitive_words = response_json['data']
+        print(','.join(sensitive_words))
