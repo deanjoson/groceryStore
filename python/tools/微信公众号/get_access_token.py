3a0a94652a3bb93c720ca422f7fa89e88744fc73
邓军 <dengjun@coolwang.cn>
2023-09-29 15:37:23 +0800

    增加错误码40164判断，提示去管理台配置IP白名单

--- python/tools/微信公众号/get_access_token.py
+++ python/tools/微信公众号/get_access_token.py
@@ -4,6 +4,8 @@
 # @time    : 2023/9/29 14:15
 # @function: 获取微信公众号访问许可
 # @version : V1.0.0
+import json
+import re
 
 import requests
 
@@ -14,4 +16,9 @@ ACCESS_TOKEN_URL = f"https://api.weixin.qq.com/cgi-bin/token?grant_type=client_c
 
 response = requests.get(ACCESS_TOKEN_URL)
 if response.status_code == 200:
-    print(response.text)
+    response_json = json.loads(response.text)
+    if response_json['errcode'] == 40164:
+        errmsg = response_json['errmsg']
+        ipv4 = re.findall(r'ip(.*?)ipv6', errmsg)[0].strip()
+        print(f"请先前往mp.weixin.qq.com配置IP白名单: {ipv4}")
+    # print(response.text)
